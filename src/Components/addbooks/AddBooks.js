cd8a7281a8797ed3a19f46440f7c4810476958b1
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-07 14:15:37 -0400

    used and installed semantic-ui-css/semantic.min.css to style

--- src/Components/addbooks/AddBooks.js
+++ src/Components/addbooks/AddBooks.js
@@ -1,6 +1,80 @@
-
+import React, { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 function AddBooks() {
+    const [description, setDescription] = useState('')
+    const [author, setAuthor] = useState('')
+    const [bookName, setBookName] =('')
+    
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const handleNameChange = (event) => {
+    setBookName(event.target.value)
+  }
+
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const newBook = {
+      id: uuid(),
+      bookName:bookName,
+      description: description,
+      author:author
+    
+    }
+    fetch(' http://localhost:8001/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBook),
+    })
+      .then((resp) => resp.json())
+      .then((newBook) => console.log(newBook))
+  }
+
+  return (
+    <div className='ui segment' onSubmit={handleChange}>
+      <form className='ui form'>
+        <div className='inline fields'>
+          <input
+            type='text'
+            name='bookName'
+            value={bookName}
+            onChange={handleNameChange}
+            required
+          />
+          <input
+            type='text'
+            name='description'
+            placeholder='Description'
+            value={description}
+            onChange={handleDescriptionChange}
+            required
+          />
+          <input
+            type='text'
+            name='author'
+            placeholder='Author'
+            value={author}
+            onChange={handleAuthorChange}
+            required
+          />
+
+        </div>
+        <button className='ui button' type='submit'>
+          Add Book
+        </button>
+      </form>
+    </div>
+  )
 }
 
 
--- src/Components/serchBook/SerchBook.js
+++ src/Components/serchBook/SerchBook.js
@@ -1,6 +1,17 @@
-
+import React from 'react'
 
 function SerchBook() {
+    return (
+        <div className='ui large fluid icon input'>
+          <input
+            type='text'
+            placeholder='Search your book'
+            onChange={onSearchChange}
+            value={search}
+          />
+          <i className='circular search link icon'></i>
+        </div>
+      )
 }
 
 
