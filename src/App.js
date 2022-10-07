84256a8c8529548ccac4d1ac934f9464f778ee04
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-10-07 12:22:52 -0400

    set the app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 
+import React from "react";
+import BookContainer from "./Components/bookContainer/BookContainer";
 
 function App() {
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment violet inverted">
+        <h2>Keep Track of The Shelf</h2>
+      </div>
+      <BookContainer />
+    </div>
+  );
 }
 
 export default App;
